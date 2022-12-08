adf121b39aa45c7d19a85f3c5205c55ee8a5a5b4
이정욱 <dlwjddnr3450@gmail.com>
2022-12-08 14:38:20 +0900

    12_8_수업내용

--- js/example.js
+++ js/example.js
@@ -1,6 +1,15 @@
-alert("?");
-function plus(a, b){
-    return a + b;
-}
+const nameH1Elemnet = document.querySelector('h1.inline');
 
-document.write(plus);
+const localName = localStorage.getItem('name');
+const connectNameElement = document.querySelector('span.red');
+if(localName) nameH1Elemnet.textContent = localName;
+nameH1Elemnet.onclick = () =>{
+    const inputName = prompt('이름을 입력해 주세요');
+
+    if(inputName){
+    localStorage.setItem('name', inputName);
+
+    nameH1Elemnet.textContent = inputName;
+    connectNameElement.textContent=inputName;
+    }else {alert('내용을 입력하세요');}
+}
